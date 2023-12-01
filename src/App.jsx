268880e32c2817a3d1eb02cc2048329a4e42a68c
Pauline Nkouane <paulinenkouane15@gmail.com>
2023-12-01 17:47:04 +0100

    :lipstick: update the UI and style files

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 2)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hello <code>Welcome</code> to my page
         </p>
       </div>
       <p className="read-the-docs">
